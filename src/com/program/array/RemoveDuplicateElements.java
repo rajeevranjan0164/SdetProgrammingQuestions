dea3f0c50e6cfdccbef9f67276f34e6a727b3471
User <ifix@ifixs-MacBook-Air.local>
2025-12-12 16:46:35 +0530

    added comment on code to understand easly

--- src/com/program/array/RemoveDuplicateElements.java
+++ src/com/program/array/RemoveDuplicateElements.java
@@ -9,16 +9,23 @@ public class RemoveDuplicateElements {
 
         int[] arr = {1, 2, 3, 4, 5, 2, 7, 8, 1};
 
+        /**
+         * Using a HashSet to store unique elements.
+         */
         Set<Integer> uniqueElements = new HashSet<>();
 
-        Set<Integer> uniqueElementsTwo = new HashSet<>();
         /*
          * Iterate through the array
          * - HashSet does not allow duplicate values.
          * - When we try to add an element that already exists, it is ignored.
          * - The order of elements in HashSet is NOT maintained.
          */
+
+        /**
+         * Add elements from the array to the HashSet.
+         */
         for (int arr1 : arr) {
+
             uniqueElements.add(arr1);
         }
         // Print unique elements (duplicates are removed)
